7f65e757f2216fbd05024ee4006f6d25c81f6e49
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-03 13:48:39 +0600

    init commit

---
+++ shared/fsWrapper.hpp
@@ -0,0 +1,37 @@
+/*
+ * Khulnasoft shared modules utils
+ * Copyright (C) 2023, Khulnasoft Ltd.
+ * Jul 10, 2023.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#ifndef _FSWRAPPER_HPP
+#define _FSWRAPPER_HPP
+
+#include <filesystem>
+
+/**
+ * @brief This class is a wrapper for the filesystem library.
+ */
+class FsWrapper
+{
+protected:
+    FsWrapper() = default;
+    virtual ~FsWrapper() = default;
+
+public:
+    /**
+     * @brief Method to check if a file exists.
+     * @param name The file name.
+     */
+    static bool exists(const std::string& name)
+    {
+        return std::filesystem::exists(name);
+    }
+};
+
+#endif /* _FSWRAPPER_HPP */
---
+++ test/component/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Khulnasoft urlRequest test component
+ * Copyright (C) 2023, Khulnasoft Ltd.
+ * July 18, 2022.
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public
+ * License (version 2) as published by the FSF - Free Software
+ * Foundation.
+ */
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
